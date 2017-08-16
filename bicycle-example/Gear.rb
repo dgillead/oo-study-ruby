7654649bfaf086a8a2c5eabdf646541a96754f32
Darnell Gillead <dlgillead@gmail.com>
2017-08-16 17:06:45 -0500

    created Wheel class, moved diameter method to Wheel class

--- bicycle-example/Gear.rb
+++ bicycle-example/Gear.rb
@@ -1,11 +1,10 @@
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
+  attr_reader :chainring, :cog, :wheel
 
-  def initialize(chainring, cog, rim, tire)
+  def initialize(chainring, cog, wheel=nil)
     @chainring = chainring
     @cog = cog
-    @rim = rim
-    @tire = tire
+    @wheel = wheel
   end
 
   def ratio
@@ -13,9 +12,6 @@ class Gear
   end
 
   def gear_inches
-    ratio * (rim + (tire * 2))
+    ratio * wheel.diameter
   end
 end
-
-puts Gear.new(52, 11, 26, 1.5).gear_inches
-puts Gear.new(52, 11, 24, 1.25).gear_inches
---
+++ bicycle-example/Wheel.rb
@@ -0,0 +1,25 @@
+require_relative './Gear.rb'
+
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+end
+
+wheel = Wheel.new(26, 1.5)
+puts wheel.circumference
+
+puts Gear.new(52, 11, wheel).gear_inches
+
+puts Gear.new(52, 11).ratio
